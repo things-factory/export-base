dd5f99568bf2c2dc7f76b3e065d5c4de6201e7bf
Hearty Oh <heartyoh@hatiolab.com>
2019-06-20 18:10:30 +0900

    v0.0.8

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,8 +1,34 @@
 import { store } from '@things-factory/shell'
+
+import { UPDATE_EXTENSION } from './actions/export'
 import exporting from './reducers/export'
 
 export default function bootstrap() {
   store.addReducers({
     exporting
   })
+
+  /* default extension JSON 을 제공함 */
+  store.dispatch({
+    type: UPDATE_EXTENSION,
+    extensions: {
+      json: {
+        export: dataToJson
+      }
+    }
+  })
+}
+
+function dataToJson({ extension, data, name }) {
+  const records = typeof data == 'function' ? data.call() : data
+  const json = JSON.stringify(records, null, 2)
+
+  const link = document.createElement('a')
+
+  link.setAttribute('href', encodeURI('data:application/json;charset=utf-8,' + json))
+  link.setAttribute('download', `${name}.json`)
+
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
 }
